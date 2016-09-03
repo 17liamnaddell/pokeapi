3498b3d0a7a058993cb3e414f1449b50ffdfe847
Liam Naddell <beanboybunny@gmail.com>
2016-09-03 10:44:29 -0400

    save

--- go/src/pokeapi/main.go
+++ go/src/pokeapi/main.go
@@ -1,12 +1,29 @@
 package main
 
 import "net/http"
-
-//import "fmt"
+import "log"
+import "fmt"
 import "io/ioutil"
+import "encoding/json"
 
 func main() {
 	poke, _ := http.Get("http://pokeapi.co/api/v2/pokemon/")
 	text, _ := ioutil.ReadAll(poke.Body)
 	ioutil.WriteFile("output.txt", text, 0666)
+	bulbas, err := http.Get("http://pokeapi.co/api/v2/pokemon/1")
+	drive, err1 := ioutil.ReadAll(bulbas.Body)
+	ioutil.WriteFile("inauth.txt", drive, 0666)
+	checkerr(err)
+	checkerr(err1)
+
+	data := map[string]interface{}{}
+	err4 := json.Unmarshal(drive, &data)
+	fmt.Println("errror: ", err4)
+	fmt.Println(data["abilities"])
+}
+
+func checkerr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
