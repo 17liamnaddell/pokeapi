e8bf3b0106eb30b51c79155196b8da93f2fc6727
Liam Naddell <beanboybunny@gmail.com>
2016-09-26 22:59:56 -0400

    save

--- go/src/pokeapi/main.go
+++ go/src/pokeapi/main.go
@@ -15,8 +15,8 @@ import (
 type Pokedata struct {
 	Abilities []struct {
 		Ability struct {
-			Name string `json:"name"`
-		} `json:"ability"`
+			Name string
+		}
 	} `json:"abilities"`
 	Name  string `json:"name"`
 	Types []struct {
@@ -27,25 +27,71 @@ type Pokedata struct {
 	Weight int `json:"weight"`
 }
 
+type Idkwtth struct {
+	Results []struct {
+		Name string `json:"name"`
+	} `json:"results"`
+	Next string `json:"next"`
+}
+
 func main() {
+	var home = os.Getenv("HOME")
+	if _, err := os.Stat(home + "/.pokeapi"); os.IsNotExist(err) == true {
+		fmt.Println(err)
+		os.Chdir(os.Getenv("HOME"))
+		err := os.Mkdir(".pokeapi", 0777)
+		fmt.Println(err)
+	}
 	go captureCC()
 	fmt.Println(os.Args)
 	for i := 1; i < len(os.Args); i++ {
 		if os.Args[i] == "-f" || os.Args[i] == "--find" {
-			pokelink := "https://pokeapi.co/api/v2/pokemon/" + os.Args[i+1]
-			fmt.Println(pokelink)
-			poke, err := http.Get(pokelink)
-			fmt.Println(poke.Body)
-			checkerr(err)
-			pokedat := Pokedata{}
-			idontcare, _ := ioutil.ReadAll(poke.Body)
-			err = json.Unmarshal(idontcare, &pokedat)
-			fmt.Println(pokedat)
-			checkerr(err)
+			os.Chdir(home + "/.pokeapi")
+			pokedataDirs, _ := os.Open("/.pokeapi")
+			myint := 0
+			dirs, _ := pokedataDirs.Readdirnames(myint)
+			for q := 0; q < len(dirs); q++ {
+				if dirs[i] == os.Args[i+1] {
+					fmt.Println("kekappa")
+				}
+			}
+
+			pokedat := GetPokemon(os.Args[i+1])
+
 			printit(pokedat)
+		} else if os.Args[i] == "-rf" || os.Args[i] == "--removeData" {
+			os.Chdir(home + "/.pokeapi")
+			os.RemoveAll(".")
+		} else if os.Args[i] == "-fa" || os.Args[i] == "--findAll" {
+			//list all pokemon
+			ListPokemon()
+			//get all data from each pokemon and write that to the file
+
 		}
 	}
 
+}
+
+func GetPokemon(name string) Pokedata {
+	pokelink := "https://pokeapi.co/api/v2/pokemon/" + name
+	fmt.Println(pokelink)
+	poke, err := http.Get(pokelink)
+	fmt.Println(poke.Body)
+	checkerr(err)
+	pokedat := Pokedata{}
+	idontcare, _ := ioutil.ReadAll(poke.Body)
+	_ = ioutil.WriteFile(name, idontcare, 0777)
+	err = json.Unmarshal(idontcare, &pokedat)
+	checkerr(err)
+	return pokedat
+}
+
+func ListPokemon() {
+	raw, err := http.Get("https://pokeapi.co/api/v2/pokemon")
+	checkerr(err)
+	rawJson, _ := ioutil.ReadAll(raw.Body)
+	poka := Idkwtth{}
+	err = json.Unmarshal(rawJson, &poka)
 }
 
 func captureCC() {
