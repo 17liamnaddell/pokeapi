8d01511c732e57c98f1f504aa94d75f9a6564cbb
Liam Naddell <liamnprg@gmail.com>
2017-07-28 14:04:02 -0400

    better gui

--- cmd/pokeapi/main.go
+++ cmd/pokeapi/main.go
@@ -4,7 +4,9 @@ import "github.com/liamnaddell/pokeapi"
 import "os"
 import "github.com/mattn/go-gtk/glib"
 import "github.com/mattn/go-gtk/gtk"
-import "fmt"
+
+//import "fmt"
+import "strconv"
 
 //import "github.com/mattn/go-gtk/gdk"
 
@@ -12,27 +14,51 @@ import "fmt"
 
 func main() {
 	pokemon := pokeapi.StartGetPokemon(os.Args[1])
-	fmt.Println(pokemon.Types[0].Type.Name)
+	//fmt.Println(pokemon.Types[0].Type.Name)
 	window := basicGtk()
 
 	//vbox/label
 	vbox := gtk.NewVBox(false, 1)
+	Tsep := gtk.NewHSeparator()
 	label := newHeader(pokemon.Name)
-	vbox.PackStart(label, false, true, 3)
+	vbox.PackStart(label, false, true, 0)
+	vbox.PackStart(Tsep, false, true, 0)
 
-	label2 := gtk.NewLabel(pokemon.Types[0].Type.Name)
+	//boxes
 	hbox := gtk.NewHBox(false, 1)
-	hbox.PackStart(label2, false, true, 0)
+
 	vbox.PackStart(hbox, false, true, 0)
-	hbox.ShowAll()
-	//sep := gtk.NewVSeparator()
+	//label 2
+	label2 := labelWMkup("<span size=\"10000\"> type:<b>  " + pokemon.Types[0].Type.Name + "  </b></span>")
+	hbox.PackStart(label2, false, true, 0)
+	sep := gtk.NewVSeparator()
+	hbox.PackStart(sep, false, true, 0)
+
+	//bottom separator
+	sep2 := gtk.NewHSeparator()
+	//vbox.PackStart(sep2, false, true, 2)
+	vbox.PackStart(sep2, false, true, 0)
+
+	//label 3
+	str := strconv.Itoa(pokemon.Id)
+	label3 := labelWMkup("<span size=\"15000\"> id:<b> " + str + " </b></span>")
+	sep3 := gtk.NewVSeparator()
+	hbox.PackEnd(label3, false, true, 0)
+	hbox.PackEnd(sep3, false, true, 0)
+
 	//vbox.Add(sep)
 	//show all
+	hbox.ShowAll()
 	vbox.ShowAll()
 	window.Add(vbox)
 	window.ShowAll()
 	gtk.Main()
 
+}
+func labelWMkup(markup string) *gtk.Label {
+	label := gtk.NewLabel("")
+	label.SetMarkup(markup)
+	return label
 }
 
 func basicGtk() *gtk.Window {
